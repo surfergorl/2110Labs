3e5a9d511bbbab8aaad77ab2b06a15c0602fa0e6
Brittany Snow <br232416@dal.ca>
2024-09-13 17:17:46 -0300

    new message new

--- LabZero2110/src/labzero/LabZero2110.java
+++ LabZero2110/src/labzero/LabZero2110.java
@@ -4,7 +4,7 @@ public class LabZero2110 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello Brittany!");
 	}
 
 }
